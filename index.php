93c5b4aa7e286697a71c0d0e88c7cf1819a554f6
FLU <valerio.massj@gmail.com>
2021-12-17 17:40:01 +0100

    update: add censored word set by user

--- index.php
+++ index.php
@@ -10,12 +10,11 @@
 <body>
     <?php
         $para = "The headphones were on. They had been utilized on purpose. She could hear her mom yelling in the background, but couldn't make out exactly what the yelling was about. That was exactly why she had put them on. She knew her mom would enter her room at any minute, and she could pretend that she hadn't heard any of the previous yelling.";
-        
     ?>
     <div class="container">
 
         <p>
-
+            <span>Paragraph:</span>
             <?php 
                 echo $para;
             ?>
@@ -23,12 +22,32 @@
         </p>
 
         <div>
-            Number of words:       
+            <span>Number of words:</span>
             <?php 
                 echo strlen($para);
             ?>
         </div>
 
+        <form action="" method="get">
+            <input type="text" name="censored" value="" placeholder="add the censored word">
+        </form>
+
+
+        <p>
+            <span>Censored Paragraph:</span>
+            <?php
+                $censored = $_GET['censored'];
+                $censo_para = str_replace($censored, "***", $para);
+                echo $censo_para;
+            ?>
+        </p>
+
+        <div>
+            <span>Number of words in censored para:</span>
+            <?php 
+                echo strlen($censo_para);
+            ?>
+        </div>
     </div>
 
 </body>
