72ed9b5ccf755526cfb0e762feba94db138b9f63
FLU <valerio.massj@gmail.com>
2021-12-17 15:12:45 +0100

    patch: paragraph length

--- index.php
+++ index.php
@@ -4,14 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Bad Words PHP</title>
 </head>
 <body>
     <?php
-        $nome = "Valerio";
+        $para = "The headphones were on. They had been utilized on purpose. She could hear her mom yelling in the background, but couldn't make out exactly what the yelling was about. That was exactly why she had put them on. She knew her mom would enter her room at any minute, and she could pretend that she hadn't heard any of the previous yelling.";
+        
     ?>
-    
-    <h1>Ciao <?php echo $nome ?></h1>
+    <div class="container">
+
+        <p>
+
+            <?php 
+                echo $para;
+            ?>
+
+        </p>
+
+        <div>
+            Number of words:       
+            <?php 
+                echo strlen($para);
+            ?>
+        </div>
+
+    </div>
 
 </body>
 </html>
